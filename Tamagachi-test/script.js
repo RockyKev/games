a16421eae82a8e0d44b608cb68479e7486a9dadf
Rocky Kev <6101650+RockyKev@users.noreply.github.com>
2019-07-06 10:41:21 -0700

    Add files via upload

---
+++ Tamagachi-test/script.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    tama: {
+    age: 0,
+    weight: 1.0,
+    food: 1,
+    play: 1, 
+    energy: 1,
+    sickness: 0, 
+    poop: 0
+    }
+  }
+})
